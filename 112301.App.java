f922672ed618e74c8979e2650a259824557b6418
Liangxunhe <74659887+Liangxunhe@users.noreply.github.com>
2020-11-23 11:22:28 +0800

    Create 112301.App.java

---
+++ 112301.App.java
@@ -0,0 +1,23 @@
+package pac;
+
+import java.util.Scanner;
+
+public class Eewei {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while (true) {
+			System.out.println("麻烦你输入个1");
+		Scanner scanner=new Scanner(System.in);
+		 int user_num=scanner.nextInt();
+		 if(user_num==1){
+			 int x=(int)(Math.random()*(100)+1);
+			 int y=(int)(Math.random()*(100)+1);
+             System.out.println(x+","+y);
+         }else {
+        	 System.out.println("是输入1呀同学");
+         }
+			
+		}
+	}
+}
