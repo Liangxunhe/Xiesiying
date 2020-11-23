16e25af65224431387b196d54bb75ab9c52162e6
Liangxunhe <74659887+Liangxunhe@users.noreply.github.com>
2020-11-23 12:01:17 +0800

    Create 112302.App.java

---
+++ 112302.App.java
@@ -0,0 +1,15 @@
+package pac;
+
+import java.util.Scanner;
+
+public class Eewei {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		while (true) {
+		Thread.sleep(1000);
+             System.out.println((Math.random()*(100))+","+(Math.random()*(100)));
+		}	
+		}
+	}
+
